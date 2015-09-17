4499c024758cd345fc55fa7572ddd00542080bb2
Jose Manuel Peralta Galisteo <josemanuelperaltagalisteo@hotmail.com>
2015-09-17 12:56:38 +0200

    Ejemplo-Demo

---
+++ DemoMvvm/ViewModels/SampleViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace DemoMvvm.ViewModels
+{
+    class SampleViewModel : VMBase
+    {
+        private string sampleText;
+
+        public SampleViewModel()
+        {
+            SampleText = "Texto sin cambiar";
+        }
+
+        public string SampleText 
+        {
+            get { return sampleText;}
+            set 
+            { 
+                sampleText = value;
+                RaisePropertyChanged("SampleText");
+            }
+        }
+    }
+
+}
---
+++ DemoMvvm/ViewModels/VMBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace DemoMvvm.ViewModels
+{
+    class VMBase : INotifyPropertyChanged
+    {
+        public VMBase()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(String propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
